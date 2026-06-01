b19f297ea25da9e6123dc97281aadcfc9397ec2d
ZuriPL <michal.bak222@gmail.com>
2026-06-01 21:48:19 +0200

    split unaligned_growth into 2 tests

--- test_generators/grow_over_page_size.py
+++ test_generators/grow_over_page_size.py
@@ -7,7 +7,7 @@ def gen_n(): # Prints to stdout exactly the characters that are to be passed to 
 @command
 def gen_stdin(): # Prints to stdout exactly the characters that are to be passed to the program as stdin.
   print("A")
-  print("A" + "a" * 3000 + "A")
+  print("A$" + "a" * 5000 + "A")
 
 if __name__ == "__main__":
   run()
---
+++ test_generators/grow_under_page_size.py
@@ -0,0 +1,13 @@
+from lib.command import command, run
+
+@command
+def gen_n(): # Prints to stdout exactly the characters that are to be passed to the program as n.
+  print("10", end='')
+
+@command
+def gen_stdin(): # Prints to stdout exactly the characters that are to be passed to the program as stdin.
+  print("A")
+  print("A$" + "a" * 3000 + "A")
+
+if __name__ == "__main__":
+  run()
